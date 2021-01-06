f99878c31fb511ee8a23fd89e6648cd8b4fbf661
EaE_2626 <imposter.kore2626@gmail.com>
2021-01-06 23:39:12 +0900

    Added a method for generating original formats.

--- QuickSpread/Client/Excel/ExcelSpreadSheetOptions.cs
+++ QuickSpread/Client/Excel/ExcelSpreadSheetOptions.cs
@@ -11,12 +11,35 @@
         internal string Format { get; set; }
     }
 
+    /// <summary>
+    /// Cell format for Datetime.
+    /// </summary>
     public class DateTimeFormat
     {
         /// <summary>
         /// Datetime fomat → yyyy/MM/dd HH:mm:ss
         /// </summary>
         public static CellFormat yyyyMMdd_HHmmss = new CellFormat() { Format = "yyyy/MM/dd HH:mm:ss" };
+
+        /// <summary>
+        /// Datetime fomat → yyyy/MM/dd
+        /// </summary>
+        public static CellFormat yyyyMMdd = new CellFormat() { Format = "yyyy/MM/dd" };
+
+        /// <summary>
+        /// Datetime fomat → yyyy/MM/dd
+        /// </summary>
+        public static CellFormat yyyyMMdd_HHmm = new CellFormat() { Format = "yyyy/MM/dd HH:mm" };
+
+        /// <summary>
+        /// Generate your own format.
+        /// </summary>
+        /// <param name="format">string format.</param>
+        /// <returns>cell format.</returns>
+        public static CellFormat CreateCellDateTimeFormat(string format)
+        {
+            return new CellFormat() { Format = format };
+        }
     }
 
     /// <summary>
